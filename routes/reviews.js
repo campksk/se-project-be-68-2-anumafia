f056c077545ddf11b428869326920f99b251afa9
Khetsopon11 <khetsoponkrongyut@gmail.com>
2026-04-11 13:33:53 +0700

    add-route-deleteReview

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { createReview } = require("../controllers/review");
+const { createReview,deleteReview } = require("../controllers/review");
 const { protect, authorize } = require("../middleware/auth");
 
 router.route("/")
     .post(protect, authorize("admin", "user"), createReview);
+
+router.route("/:id")
+    .delete(protect,authorize("admin","user"),deleteReview);
 
 module.exports = router;
