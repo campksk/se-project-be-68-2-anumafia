0a568dd08b126a78e7a1115feddf99041a61f100
camp_.ksk <camp131249@gmail.com>
2026-04-13 14:03:32 +0700

    Implement getUsers

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+
+// @desc    get list of all user
+// @route   GET /api/v1/users
+// @access  Private
+exports.getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            success: true,
+            count: users.length,
+            data: users
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: 'Server Error'
+        });
+    }
+};
